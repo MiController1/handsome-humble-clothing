3c33eb32de65fba95ea666a222939a3c3539ad4c
MiController1 <you@example.com>
2024-06-13 12:46:22 -0700

    modified context nav firebase button sign in sign up styles components

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -6,7 +6,6 @@ import Button from "../button/button.component";
 
 import {
   signInWithGooglePopUp,
-  createUserDocumentFromAuth,
   signInAuthUserWithEmailAndPassword,
 } from "../../utils/firebase/firebase.utils";
 
@@ -26,25 +25,21 @@ const SignInForm = () => {
   };
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopUp();
-    await createUserDocumentFromAuth(user);
+    await signInWithGooglePopUp();
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await signInAuthUserWithEmailAndPassword(
-        email,
-        password
-      );
-      console.log(response);
+      await signInAuthUserWithEmailAndPassword(email, password);
+
       resetFormFields();
     } catch (error) {
       switch (error.code) {
-        //  case "auth/invalid-credential":
-        //  alert("incorrect password for email");
-        // break;
+        case "auth/popup-closed-by-user":
+          alert("Google sign in cancelled");
+          break;
         case "auth/invalid-credential":
           alert("no user associated with this email or incorrect password");
           break;
@@ -60,7 +55,7 @@ const SignInForm = () => {
   };
 
   return (
-    <div className="sign-up-container">
+    <div className="sign-in-container">
       <h2>Already have an account?</h2>
 
       <span>Sign In with Your Email and Password</span>
@@ -83,8 +78,8 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
-            Google Sign In
+          <Button buttonType="google" type="button" onClick={signInWithGoogle}>
+            Sign In With Google
           </Button>
         </div>
       </form>
