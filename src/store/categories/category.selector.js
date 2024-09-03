828bde978dd25dcf65fc961f52f1a2c370205172
MiController1 <you@example.com>
2024-09-02 19:10:00 -0700

    modified categories folder firebase utils shop category components  added reselect and memoization into category selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,1 +1,22 @@
-export const selectCategoriesMap = (state) => state.categories.categoriesMap;
+import { createSelector } from "reselect";
+
+// Input selector: extracts the part of the state you're interested in
+const selectCategoryReducer = (state) => state.categories;
+
+// Output selector: uses createSelector to create a memoized selector
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories], // Input selectors
+  (
+    categories // Transform function
+  ) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
