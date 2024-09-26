3d234b679759f5d139488fbe15c06e5f20385034
MiController1 <you@example.com>
2024-09-26 13:24:03 -0700

    modified routes checkout category navigation store root reducer added new store cartfiles

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useContext } from "react";
+import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 
 import { useSelector } from "react-redux";
@@ -6,9 +6,9 @@ import { useSelector } from "react-redux";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
-import { selectCurrentUser } from "../../store/user.selector";
+import { selectIsCartOpen } from "../../store/cart/cart.selector";
 
-import { CartContext } from "../../contexts/cart.context";
+import { selectCurrentUser } from "../../store/user.selector";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
@@ -22,7 +22,7 @@ import {
 
 const Navigation = () => {
   const currentUser = useSelector(selectCurrentUser);
-  const { isCartOpen } = useContext(CartContext);
+  const isCartOpen = useSelector(selectIsCartOpen);
 
   return (
     <Fragment>
